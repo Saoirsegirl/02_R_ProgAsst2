474a90a8070be9dd95d5f51fa096a5392efac427
Saoirsegirl <Saoirsegirl@gmail.com>
2014-09-20 00:34:07 -0700

    Final submit for Peer review

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,14 @@
-## The following program will ingest an matrix of data and "cache" it's inverse to
-## make it available for quicker access during repetative calculations within another
-## function.
+## The following is a 2-step program to ingest a matrix of data, "cache" it's inverse
+## and make it available for quicker access during repetitive calculations.
 
-## This function takes in a matrix of contents, creates several several internal
-## functions, and using those functions creates cached variable that are ready for
-## use by the function cacheSolve or other functions needing quick access to the
-## inverse of the same matrix.  Note: the matrix should be square to work consistantly.
+## The first function takes in a matrix of contents, creates several internal
+## functions, and using those functions creates cached variables that are ready for
+## use by the function cacheSolve() or other functions needing quick access to the
+## inverse of the matrix defined in the function(arguements). 
+## To use: Create an object with this function. Note: the matrix must be square to work.
+        ## ex: ma <- makeCacheMatrix(x = your matrix' defintion) 
 
 makeCacheMatrix <- function(x = matrix()) {
-        #makeVector <- function(x = numeric()) {
         inv <- NULL
         set <- function(y) {
                 x <<- y
@@ -22,11 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
                      getinv = getinv)
 }
 
-## Write a short comment describing this function
+## Step 2: evaluates if the cache exists, and if not, populates it with the solve()
+## created inverse to the matrix defined in the first function.
+## Call the cached data using the object of the fist function. i.e. cacheSolve(ma)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        #cachemean <- function(x, ...) {
                 inv <- x$getinv()
                 if(!is.null(inv)) {
                         message("getting cached data")
@@ -37,3 +37,7 @@ cacheSolve <- function(x, ...) {
                 x$setinv(inv)
                 inv
 }
+
+## To test if this program is operating properly, create the inverse of the first matrix
+## x = argument, using ma2 <- cacheSolve(ma). print out ma2. Then run makeCacheMatrix(x = ma2),
+## and create ma3 <- cacheSolve(ma2).  ma3 should match the original matrix.
